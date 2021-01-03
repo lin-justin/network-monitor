019249f84b326f7b58157ed8bdccb09ffdbe95dd
Justin Lin <justinhlin@yahoo.com>
2021-01-02 16:18:25 -0800

    Add function to download file from HTTPS URL

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 class ConanPackage(ConanFile):
     name = "network-monitor"
@@ -8,6 +8,7 @@ class ConanPackage(ConanFile):
 
     requires = [
         ("boost/1.74.0"),
+        ("libcurl/7.73.0"),
         ("openssl/1.1.1h"),
     ]
 
